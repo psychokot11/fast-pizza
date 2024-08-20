e6a7a705c83a7ce9d51d7f3ac961a802fdf899cb
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-20 20:52:33 +0200

    loader component

--- fast-pizza/src/ui/AppLayout.jsx
+++ fast-pizza/src/ui/AppLayout.jsx
@@ -1,17 +1,22 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import Header from "./Header"
 import CartOverview from "../features/cart/CartOverview"
+import Loader from "./Loader"
 
 function AppLayout() {
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
+    console.log('isLoading:', isLoading)
+
     return (
-        <>
+        <div className="layout">
+            {isLoading && <Loader />}
             <Header />
             <main>
-                <h1>Content</h1>
                 <Outlet />
             </main>
             <CartOverview />
-        </>
+        </div>
     )
 }
 
