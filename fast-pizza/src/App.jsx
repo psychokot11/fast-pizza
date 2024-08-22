0cc6b09a5a42a84bee77234df3f7037ef602f788
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-22 21:42:11 +0200

    create order form

--- fast-pizza/src/App.jsx
+++ fast-pizza/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './ui/Home'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import Order , { loader as orderLoader }from './features/order/Order'
-import CreateOrder from './features/order/CreateOrder'
+import CreateOrder, { action as createOrderAction} from './features/order/CreateOrder'
 
 const router = createBrowserRouter([
   {
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
       {
         path: 'order/new',
         element: <CreateOrder />,
-        errorElement: <Error />,
+        action: createOrderAction,
       },
       {
         path: 'order/:orderId',
