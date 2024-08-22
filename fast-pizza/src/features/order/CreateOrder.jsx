6f0e6a1675e2fa0bdbbcd69768723ac109143a13
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-22 22:19:45 +0200

    error handling

--- fast-pizza/src/features/order/CreateOrder.jsx
+++ fast-pizza/src/features/order/CreateOrder.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import { Form, redirect } from "react-router-dom"
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom"
 import { createOrder } from "../../services/apiRestaurant"
 
 // // https://uibakery.io/regex-library/phone-number
-// const isValidPhone = (str) =>
-//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-//     str
-//   );
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
 
 const fakeCart = [
   {
@@ -33,6 +33,11 @@ const fakeCart = [
 ]
 
 function CreateOrder() {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
+  const formErrors = useActionData()
+
   const [withPriority, setWithPriority] = useState(false)
   const cart = fakeCart
 
@@ -51,6 +56,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -76,7 +82,9 @@ function CreateOrder() {
             type="hidden"
             name="cart"
             value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>
@@ -91,6 +99,16 @@ export async function action({ request }) {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === 'on'
+  }
+
+  const errors = {}
+
+  if (!isValidPhone(order.phone)) {
+    errors.phone = 'Invalid phone number'
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return errors
   }
   
   const newOrder = await createOrder(order)
