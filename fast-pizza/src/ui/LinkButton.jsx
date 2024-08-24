7528588f6d589007566b7b3f7b2039c9b692f561
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-24 20:32:41 +0200

    style with tailwind

---
+++ fast-pizza/src/ui/LinkButton.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import { Link, useNavigate } from "react-router-dom";
+
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  const className = "text-blue-500 text-sm hover:text-blue-600 hover:underline";
+
+  if (to === "-1")
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
+
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
