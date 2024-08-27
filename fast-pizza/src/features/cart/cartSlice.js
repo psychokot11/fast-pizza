bf40058429e7922a2f7e2f5ab0cfac4d76c3f13b
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-27 21:48:51 +0200

    cart slice

---
+++ fast-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuntity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    decreaseItemQuntity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuntity,
+  decreaseItemQuntity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
