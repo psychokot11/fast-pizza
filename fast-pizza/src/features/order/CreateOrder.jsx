edf86b347c12ce71ecb7bece22523606d966567c
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-31 21:37:23 +0200

    integrate geolocation

--- fast-pizza/src/features/order/CreateOrder.jsx
+++ fast-pizza/src/features/order/CreateOrder.jsx
@@ -19,7 +19,8 @@ const isValidPhone = (str) =>
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
-  const userName = useSelector((state) => state.user.username);
+  const { username, status: addressStatus, position, address, error: errorAddress} = useSelector((state) => state.user);
+  const isLoadingPositionAddress = addressStatus === "loading";
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
@@ -32,7 +33,7 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-      <button onClick={() => dispatch(fetchAddress())}>get location</button>
+      
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
@@ -42,7 +43,7 @@ function CreateOrder() {
             className="input grow"
             type="text"
             name="customer"
-            defaultValue={userName}
+            defaultValue={username}
             required
           />
         </div>
@@ -59,16 +60,34 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center relative">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              defaultValue={address}
+              disabled={isLoadingPositionAddress}
               required
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+              {errorAddress}
+            </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && 
+            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+              <Button 
+                type="small" 
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress())
+                  }}
+                disabled={isLoadingPositionAddress}>get location</Button>
+            </span>
+          }
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -87,7 +106,8 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <input type="hidden" name="position" value={position.latitude && position.longitude ? `${position.latitude}, ${position.longitude}` : ''} />
+          <Button disabled={isSubmitting || isLoadingPositionAddress} type="primary">
             {isSubmitting ? "Placing order...." : `Order now ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
